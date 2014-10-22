2ad6d1bcd479b774b9b88a5234251b2ecbb69232
Ziz <zizsdl@googlemail.com>
2014-10-22 12:41:22 +0200

    New version because of new sparrowNet version

--- make_release_packages.sh
+++ make_release_packages.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 # Change the folder to YOUR sparrow3d folder!
 PROGRAM="Sparrow-C4A-Manager"
-VERSION="1.0.4.1"
+VERSION="1.0.4.2"
 DEST=./build/*
 echo "<html>" > index.htm
 echo "<head>" >> index.htm
